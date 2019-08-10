c7936dc7175bc061fae423275ee1cc991ba22ba3
rguliev <glvrst@gmail.com>
2019-08-10 17:59:57 +0300

    Bug fixes and minor improvements in

--- egs/hqa/helpers/gop.py
+++ egs/hqa/helpers/gop.py
@@ -13,32 +13,40 @@ values are probability for the corresponding phoneme to be at the frame with res
 
 import os
 import glob
-
+from typing import Optional
 import numpy as np
 import pandas as pd
 
-from kaldi_io import phone_symb2int, pdf2phone, read_ali_from_stdout, read_feats_from_stdout
+from kaldi_io import phone_symb2int, pdf2phone, read_ali, read_feats
 from utils import softmax
 
-# Set environment
-# load_dotenv('./.env', verbose=True, override=True)
 
-def compute_gmm_probs(model_dir: str, feats_rspec: str, ali_rspec: str = None) -> pd.DataFrame:
+def compute_gmm_probs(
+    model_dir: str, feats_rspec: str, ali_rspec: Optional[str] = None
+) -> pd.DataFrame:
     """Compute GOP probs for a GMM based model
 
     Computes probs for GOP using a GMM model. The algorithm is (for each utterance):
-    1. Estimate 'real' phonemes for each frame by alignment (by `ali-to-phones --per-frame=true <model> <ali_rspec> ark,t:-`):
+    1. Estimate 'real' phonemes for each frame by alignment
+        (by `ali-to-phones --per-frame=true <model> <ali_rspec> ark,t:-`):
         Output: Vector of length M, M - number of frames
-    2. Compute log-likelihoods of pdf_ids for each frame (by `gmm-compute-likes {model} {feats_rspec} ark,t:-`)
+    2. Compute log-likelihoods of pdf_ids for each frame
+        (by `gmm-compute-likes {model} {feats_rspec} ark,t:-`)
         Output: Matrix MxN, M - number of frames, N - number of pdf_ids
-    3. Convert log-likelihoods to probabilities by softmax transformation: probs[i,:] = softmax(likes[i,:])
+    3. Convert log-likelihoods to probabilities by softmax transformation:
+        probs[i,:] = softmax(likes[i,:])
         Output: Matrix MxN, M - number of frames, N - number of pdf_ids.
-    4. Map pdf_id to phonemes by `show-transitions {model_path}/phones.txt {model_path}/final.mdl {model_path}/final.occs | grep Transition-state`
-    5. Sum pdf_id-probabilities by phoneme: probs_new[i,j] = sum(probs[i, <all pdf_ids of phoneme j>])
+    4. Map pdf_id to phonemes by `show-transitions`
+        (see kaldi_io.pdf2phone for details)
+    5. Sum pdf_id-probabilities by phoneme:
+        probs_new[i,j] = sum(probs[i, <all pdf_ids of phoneme j>])
         Output: Matrix MxK, K - number of phonemes
-    6. For each frame get: 
-        max(probs_new[i,:]), argmax(probs_new[i,:]), probs_new[i,alignment_phoneme[i]], alignment_phoneme[i]
-        This is one row for output DataFrame
+    6. For each frame get:
+        - predicted_phone = argmax(probs_new[i,:]),
+        - predicted_phone_prob = max(probs_new[i,:]),
+        - real_phone = alignment_phoneme[i]
+        - real_phone_prob = probs_new[i,alignment_phoneme[i]],
+        This is one row of the output DataFrame
 
     Parameters
     ----------
@@ -47,7 +55,7 @@ def compute_gmm_probs(model_dir: str, feats_rspec: str, ali_rspec: str = None) -
     feats_rspec: str
         A rspec for feats to provide for gmm-compute-likes. For example,
         feats_rspec = f'"ark,s,cs:apply-cmvn --utt2spk=ark:{data}/utt2spk scp:{data}/cmvn.scp scp:{data}/feats.scp ark:- | add-deltas ark:- ark:- |"'
-    ali_rspec: str,None
+    ali_rspec: str, optional
         Standard kaldi rspec for an alignment file. For example, 'ark:"gunzip -c exp/mono_mfcc/ali.1.gz|"
         By default all ali.*.gz files from model_dir are taken.
 
@@ -55,11 +63,12 @@ def compute_gmm_probs(model_dir: str, feats_rspec: str, ali_rspec: str = None) -
     -------
     probs: pd.DataFrame
         A pandas DataFrame of following structure:
-        - utterance - name of utterance
-        - max_prob - maximum phoneme-probability for the corresponding frame
-        - max_phone - a phoneme having maximum probability for the frame (i.e. argmax for probability)
-        - ali_prob - probability of a phoneme expected by alignment
-        - ali_phone - a phoneme expected by alignment
+          - utterance - name of utterance
+          - frame - number of the frame
+          - predicted_phone - a phoneme having maximum probability for the frame (i.e. argmax for probability)
+          - predicted_phone_prob - maximum phoneme-probability for the corresponding frame
+          - real_phone - a phoneme expected by alignment
+          - real_phone_prob - probability of the phoneme expected by the alignment
     """
     model = os.path.join(model_dir, "final.mdl")
     phones_file = os.path.join(model_dir, "phones.txt")
@@ -73,20 +82,11 @@ def compute_gmm_probs(model_dir: str, feats_rspec: str, ali_rspec: str = None) -
     pdf2int = {k: symb2int[v] for k, v in pdf2symb.items()}
 
     # Get alignments
-    if ali_rspec in None:
-        # By default get alignments from all ali.*.gz files in the model_dir
-        ali_files = sorted(glob.glob(os.path.join(model_dir, 'ali.*.gz')))
-        alis = {}
-        for ali_file in ali_files:
-            alis.update(
-                read_ali_from_stdout(model, f'ark:"gunzip -c {ali_file}|"')
-                )
-    else:
-        alis = read_ali_from_stdout(model, ali_rspec)
+    alis = read_ali(model, ali_rspec)
 
     # Compute gmm probs for each pdf id
     gmm_likes_command = f"gmm-compute-likes {model} {feats_rspec} ark,t:-"
-    gmm_likes = read_feats_from_stdout(gmm_likes_command)
+    gmm_likes = read_feats(gmm_likes_command)
 
     # Calculate GOP for each utterance
     probs_summary = {}
@@ -102,7 +102,7 @@ def compute_gmm_probs(model_dir: str, feats_rspec: str, ali_rspec: str = None) -
 
         # Switch from pdf_id to phoneme: sum probs by phoneme
         prob = pd.DataFrame(data=prob, columns=pdf2int.values())
-        prob = prob.groupby(by=df.columns, axis=1).sum()
+        prob = prob.groupby(by=prob.columns, axis=1).sum()
 
         # Get probs summary:
         #   max probability phoneme (i.e. predicted phoneme)
@@ -110,12 +110,14 @@ def compute_gmm_probs(model_dir: str, feats_rspec: str, ali_rspec: str = None) -
         #   alignment phoneme (i.e. real phoneme)
         probs_summary[utterance] = pd.DataFrame(
             {
-                "max_prob": prob.max(axis=1),
-                "max_phone": prob.idxmax(axis=1),
-                "ali_prob": prob.lookup(np.arange(prob.shape[0]), ali),
-                "ali_phone": ali,
+                "predicted_phone": prob.idxmax(axis=1),
+                "predicted_phone_prob": prob.max(axis=1),
+                "real_phone": ali,
+                "real_phone_prob": prob.lookup(np.arange(prob.shape[0]), ali),
             }
         )
+        probs_summary[utterance].reset_index(inplace=True)
+        probs_summary[utterance].rename({"index": "frame"}, axis=1, inplace=True)
     del gmm_likes
 
     # Convert dict of DataFrames to one DataFrame
